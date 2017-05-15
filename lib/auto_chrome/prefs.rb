2b2fa0004120c040e4a43cd6ff3c365131ceb05b
Ryan Koppenhaver <ryan.koppenhaver@nccgroup.trust>
2017-05-15 13:14:26 -0700

    misc cleanup

--- lib/auto_chrome/prefs.rb
+++ lib/auto_chrome/prefs.rb
@@ -95,11 +95,14 @@ class AutoChrome
     # - Chrome recursively ignores empty arrays and objects.  This is not
     #   implemented, so avoid those.
     # - Chrome outputs null values as the empty string.  There's code for that,
-    #   but it's untested.
+    #   but it's untested.  (XXX- maybe not true, based on expected output in pref_hash_calculator.unittest)
     # - I'm also unclear on if ordering is always alphabetical; we do that on
     #   insert (see the call to normalize), which seems to work.
     #
     # see ValueAsString in Chromium's pref_hash_calculator.cc
+    #
+    # see kTrackedPrefs in chrome_pref_service_factory.cc, and Settings.TrackedPreference* in chrome:histograms
+    # for debugging
     def serialize(value)
       return '' if value.nil?
       value.to_json
