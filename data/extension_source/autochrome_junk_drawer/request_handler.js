5c750074032f1dcd139ae437140eae3d3c08a050
dwalter0 <ds_walters@hotmail.com>
2022-09-07 06:30:00 -0400

    Updated request_handler.js so it doesn't delete the bookmarks

--- data/extension_source/autochrome_junk_drawer/request_handler.js
+++ data/extension_source/autochrome_junk_drawer/request_handler.js
@@ -18,50 +18,31 @@ function getProfileNameFromBookmark() {
 
             console.log("Saving profile name \"" + name + "\" to storage");
             chrome.storage.local.set({profile_name: name});
-            chrome.bookmarks.remove(bm.id);
+            //chrome.bookmarks.remove(bm.id);
         } else {
             getProfileNameFromStorage();
         }
     });
 }
 
-getProfileNameFromBookmark();
 
-var requestFilter = {
-        urls: [ "<all_urls>" ]
-    },
-    // The 'extraInfoSpec' parameter modifies how Chrome calls your
-    // listener function. 'requestHeaders' ensures that the 'details'
-    // object has a key called 'requestHeaders' containing the headers,
-    // and 'blocking' ensures that the object your function returns is
-    // used to overwrite the headers
-    extraInfoSpec = ['requestHeaders','blocking','extraHeaders'],
-    // Chrome will call your listener function in response to every
-    // HTTP request
-    handler = function( details ) {
-        if (typeof profile != "string") {
-            return;
-        }
 
-        var headers = details.requestHeaders,
-            blockingResponse = {};
+document.addEventListener('DOMContentLoaded', function() {
+    getProfileNameFromBookmark();
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+        function(details) {
+            if (typeof profile == "string") {
+                var headers = details.requestHeaders;
 
-        // Each header parameter is stored in an array. Since Chrome
-        // makes no guarantee about the contents/order of this array,
-        // you'll have to iterate through it to find for the
-        // 'User-Agent' element
-        for( var i = 0, l = headers.length; i < l; ++i ) {
-            if( headers[i].name == 'User-Agent' ) {
-                headers[i].value += " autochrome/" + profile;
-                break;
+                for (var i = 0; i < headers.length; i++) {
+                    if (headers[i].name == 'User-Agent') {
+                        headers[i].value += " autochrome/" + profile;
+                    }
+                }
+
+                return {requestHeaders: headers};
             }
-            // If you want to modify other headers, this is the place to
-            // do it. Either remove the 'break;' statement and add in more
-            // conditionals or use a 'switch' statement on 'headers[i].name'
-        }
-
-        blockingResponse.requestHeaders = headers;
-        return blockingResponse;
-    };
-
-chrome.webRequest.onBeforeSendHeaders.addListener( handler, requestFilter, extraInfoSpec );
+        },
+        {urls: ["*://*/*"]},
+        ['blocking', 'requestHeaders']);
+});
